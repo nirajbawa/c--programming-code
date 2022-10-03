d24e9c778138195b9cdf42d386f6a8f83070d2ae
niraj bava <nirajbava222@gmail.com>
2022-10-03 22:20:17 +0530

    origin master

---
+++ 4-arrays/1-problem-of-array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// array are container which is used to store multiple values in one variable
+
+int main()
+{
+    cout << "hello";
+    return 0;
+}
+
